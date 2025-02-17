7825872aa91c0313651afb878e9f7afc56eadefe
AMD-93 <amoyad93@gmail.com>
2025-02-17 12:01:39 +0100

    add router

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="yay" />
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+</template>
