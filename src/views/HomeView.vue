f5ae628d2f11aca13d9b7db0b83a354dc76fb314
AMD-93 <amoyad93@gmail.com>
2025-02-24 11:54:35 +0100

    carousel stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import TrailerCard from '@/components/TrailerCard.vue'
-import UpcomingCarousel from '@/components/UpcomingCarousel.vue'
+import Carousel from '@/components/CarouselComponent.vue'
 </script>
 
 <template>
@@ -11,8 +11,16 @@ import UpcomingCarousel from '@/components/UpcomingCarousel.vue'
       plot="During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches and spirits, and where humans are changed into beasts."
       year="2001"
     />
-    <UpcomingCarousel />
+    <div class="carousels"><Carousel /> <Carousel /></div>
   </main>
 </template>
 
-<style></style>
+<style>
+.carousels {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: auto;
+  width: 80%;
+}
+</style>
