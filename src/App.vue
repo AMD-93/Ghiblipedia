9c4f9ee062a60a46b2dd24fde787a519826ca8e1
AMD-93 <amoyad93@gmail.com>
2025-03-05 11:39:24 +0100

    youtube api

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
+let videoSection = document.querySelector('section')
+fetch(
+  'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLrMoWLZPWpBU8qR-2hp90obIEEAelORR1&key=AIzaSyCkeEknyIEzqzvAlqx677KxdAwkf0nKZd4',
+)
+  .then((res) => res.json())
+  .then((data) => {
+    data.items.forEach((el: { snippet: { title: string } }) => {
+      videoSection += el.snippet.title
+    })
+
+    console.log(data.items)
+  })
 </script>
 
 <template>
   <NavBar />
   <RouterView />
+  <section>
+    <div>
+      {{ videoSection }}
+    </div>
+  </section>
 </template>
 
 <style>
