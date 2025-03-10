530dbd88a4f69e0749454705bcebc32a636d7761
AMD-93 <amoyad93@gmail.com>
2025-03-10 11:59:36 +0100

    fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import NavBar from './components/NavBar.vue'
-let videoSection = document.querySelector('section')
+
+const videos = ref(null)
+
 fetch(
   'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLrMoWLZPWpBU8qR-2hp90obIEEAelORR1&key=AIzaSyCkeEknyIEzqzvAlqx677KxdAwkf0nKZd4',
 )
-  .then((res) => res.json())
-  .then((data) => {
-    data.items.forEach((el: { snippet: { title: string } }) => {
-      videoSection += el.snippet.title
-    })
+  .then((response) => response.json())
+  .then((data) => (videos.value = data))
+console.log(videos)
 
-    console.log(data.items)
-  })
+// let videoSection = useTemplateRef('video-section')
+
+// fetch(
+//   'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLrMoWLZPWpBU8qR-2hp90obIEEAelORR1&key=AIzaSyCkeEknyIEzqzvAlqx677KxdAwkf0nKZd4',
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     data.items.forEach((el: { snippet: { title: string } }) => {
+//       videoSection += el.snippet.title
+//     })
+
+//     console.log(data.items)
+//     onMounted(() => {
+//       videoSection.value?.focus()
+//     })
+//   })
 </script>
 
 <template>
@@ -19,7 +34,10 @@ fetch(
   <RouterView />
   <section>
     <div>
-      {{ videoSection }}
+      <!-- <p ref="video-section">{{ videoSection }}</p> -->
+    </div>
+    <div v-if="videos">
+      <h1>{{ videos }}</h1>
     </div>
   </section>
 </template>
@@ -29,3 +47,5 @@ body {
   background-color: #ffeee2;
 }
 </style>
+
+continue at 1514 youtube
