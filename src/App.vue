4a4ef9e13b611fc5a7106ff6246697201f7a823b
AMD-93 <amoyad93@gmail.com>
2025-03-11 11:47:37 +0100

    OMDb

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 import NavBar from './components/NavBar.vue'
 
-const videos = ref(null)
+// const videos = ref(null)
 
-fetch(
-  'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLrMoWLZPWpBU8qR-2hp90obIEEAelORR1&key=AIzaSyCkeEknyIEzqzvAlqx677KxdAwkf0nKZd4',
-)
-  .then((response) => response.json())
-  .then((data) => (videos.value = data))
-console.log(videos)
+// fetch(
+//   'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLrMoWLZPWpBU8qR-2hp90obIEEAelORR1&key=AIzaSyCkeEknyIEzqzvAlqx677KxdAwkf0nKZd4',
+// )
+//   .then((response) => response.json())
+//   .then((data) => (videos.value = data))
+// console.log(videos)
 
 // let videoSection = useTemplateRef('video-section')
 
@@ -32,14 +32,14 @@ console.log(videos)
 <template>
   <NavBar />
   <RouterView />
-  <section>
+  <!-- <section>
     <div>
-      <!-- <p ref="video-section">{{ videoSection }}</p> -->
+      <p ref="video-section">{{ videoSection }}</p>
     </div>
     <div v-if="videos">
       <h1>{{ videos }}</h1>
     </div>
-  </section>
+  </section> -->
 </template>
 
 <style>
