58a651829562b1d7aa604389dda2e4914c406c95
AMD-93 <amoyad93@gmail.com>
2025-05-14 11:50:50 +0200

    browse stuff; started reusable film card component

---
+++ src/components/FilmCard.vue
@@ -0,0 +1,136 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+interface Film {
+  title: string
+  year: string
+  plot: string
+  genres: string
+  tags: string
+}
+
+const props = defineProps<{ films: Film[] }>()
+
+const active = ref(0)
+function expandCard(index: number) {
+  active.value = index
+}
+</script>
+
+<template>
+  <div class="card-container" v-for="(film, index) in props.films" :key="index">
+    <div :class="{ active: active === index }" class="title-section" @click="expandCard(index)">
+      <h1>{{ film.title }}</h1>
+    </div>
+    <div class="info-section">
+      <h3>{{ film.year }}</h3>
+      <p>{{ film.plot }}</p>
+    </div>
+    <div class="categorize-section">
+      <div class="genres-section">
+        <h3>Genres</h3>
+        <ul>
+          <li>Genre 1</li>
+          <li>Genre 2</li>
+          <li>Genre 3</li>
+        </ul>
+      </div>
+      <div class="tags-section">
+        <h3>Tags</h3>
+        <ul>
+          <li>tag 1</li>
+          <li>tag 2</li>
+          <li>tag 3</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-container {
+  background-color: #b7b7a4;
+  padding: 5px;
+  margin: 20px;
+  border-radius: 10px;
+  height: calc(100% / 10);
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+/* .title-section {
+  background-color: aliceblue;
+}
+
+.info-section {
+  background-color: aqua;
+}
+
+.categorize-section {
+  background-color: aquamarine;
+}
+
+.genres-section {
+  background-color: beige;
+}
+
+.tags-section {
+  background-color: bisque;
+} */
+
+@media only screen and (max-width: 480px) {
+  .card-container {
+    display: flex;
+    flex-direction: column;
+    padding: 5px;
+    margin: 20px;
+    border-radius: 10px;
+  }
+  .title-section {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    padding: 5px;
+    margin: 0 5 5 0;
+  }
+  h2 {
+    margin-left: 5px;
+  }
+
+  .info-section {
+    padding: 5px;
+    margin: 5px;
+  }
+  .categorize-section {
+  }
+  .genres-section {
+    padding: 5px;
+    margin: 5px;
+  }
+  .tags-section {
+    padding: 5px;
+    margin: 5px;
+  }
+}
+@media only screen and (min-width: 481px) {
+}
+@media only screen and (min-width: 769px) {
+}
+@media only screen and (min-width: 992px) {
+  .card-container {
+    width: 80%;
+    margin: auto;
+    margin-top: 10px;
+  }
+}
+@media only screen and (min-width: 1025px) {
+  .card-container {
+    width: 80%;
+    margin: auto;
+    margin-top: 10px;
+  }
+}
+</style>
--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -1,21 +1,46 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import FilmCard from '@/components/FilmCard.vue'
 const input = ref('')
-const films = ['My Neighbor Totoro', 'Princess Mononoke', 'Spirited Away']
+const films = [
+  {
+    title: 'My Neighbor Totoro',
+    year: '1988',
+    plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis leo sapien. Nullam vehicula consequat elit, ac vulputate purus finibus ut. Sed ante nibh, consequat eu rhoncus ac, euismod a nibh. Morbi tempus mi a rhoncus molestie. Donec scelerisque risus dignissim nisi mollis eleifend.',
+    genres: 'genres',
+    tags: 'tags',
+  },
+  {
+    title: 'Spirited Away',
+    year: '2001',
+    plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis leo sapien. Nullam vehicula consequat elit, ac vulputate purus finibus ut. Sed ante nibh, consequat eu rhoncus ac, euismod a nibh. Morbi tempus mi a rhoncus molestie. Donec scelerisque risus dignissim nisi mollis eleifend.',
+    genres: 'genres',
+    tags: 'tags',
+  },
+  {
+    title: 'Princess Mononoke',
+    year: '1997',
+    plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis leo sapien. Nullam vehicula consequat elit, ac vulputate purus finibus ut. Sed ante nibh, consequat eu rhoncus ac, euismod a nibh. Morbi tempus mi a rhoncus molestie. Donec scelerisque risus dignissim nisi mollis eleifend.',
+    genres: 'genres',
+    tags: 'tags',
+  },
+]
 function filteredList() {
-  return films.filter((film) => film.toLowerCase().includes(input.value.toLowerCase()))
+  return films.filter((film) =>
+    [film.title, film.genres, film.tags].some((field) =>
+      field.toLowerCase().includes(input.value.toLowerCase()),
+    ),
+  )
 }
 </script>
 
 <template>
   <div class="container">
-    <input type="text" v-model="input" placeholder="Search..." class="input" />
+    <input type="text" v-model="input" placeholder="Search films..." class="input" />
     <div class="items-container">
-      <div class="items" v-for="film in filteredList()" :key="film">
-        <p>{{ film }}</p>
-      </div>
+      <div class="items" v-for="film in filteredList()" :key="film.title"></div>
     </div>
-
+    <FilmCard :films="filteredList()" />
     <div class="error" v-if="input && !filteredList().length">
       <p>No results found!</p>
     </div>
