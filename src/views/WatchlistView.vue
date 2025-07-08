9832075896e37d2c219b6ae0c279b997b937d354
AMD-93 <amoyad93@gmail.com>
2025-07-08 12:20:13 +0200

    broken shit

--- src/views/WatchlistView.vue
+++ src/views/WatchlistView.vue
@@ -1,34 +1,35 @@
 <script setup lang="ts">
-// import axios from 'axios'
-// import type { FilmDB } from '@/types.ts'
-// import { defineStore } from 'pinia'
-// import type { ComponentInternalInstance } from 'vue'
+import axios from 'axios'
+import type { FilmDB } from '@/types.ts'
+import { defineStore } from 'pinia'
+import type { ComponentInternalInstance } from 'vue'
 
-// user inputs film title and presses submit -> form data gets passed to POST request as a parameter (?) and gets appended to the URL (???) -> axios sends request and somehow populates DB (idk how this part happens tho)
+// user inputs film title and presses submit -> form data gets passed to POST request as a parameter (?) and gets appended to the URL (???) -> axios sends request and somehow populates DB (idk how this part happens tho) confusion of the highest ordah
 // how do I make this happen? do I need lifecycle hooks at all or nah, string literals, what else?
 // https://medium.com/@reginafrankjames30/requests-with-vuejs-and-axios-b8a9ea682f64 this might be something
+const props = defineProps<{ films: FilmDB[] }>()
 
-// function onUpdated(callback: () => void, target?: ComponentInternalInstance | null): void)
+function onUpdated(callback: () => void, target?: ComponentInternalInstance | null): void)
 
-// export const useFilmsStore = defineStore('films', {
-//   state: (): { films: FilmDB[] } => ({
-//     films: [],
-//   }),
+export const useFilmsStore = defineStore('films', {
+  state: (): { films: FilmDB[] } => ({
+    films: [],
+  }),
 
-//   actions: {
-//     async postFilm(filmTitle: string) {
-//       console.log('postFilm called')
+  actions: {
+    async postFilm(filmTitle: string) {
+      console.log('postFilm called')
 
-//       try {
+      try {
 
-//         const newFilm = await axios.post(`https://ghiblipediaapi.onrender.com/api/movies/omdb/${filmTitle}`)
-//         this.films = newFilm.data
-//       } catch (error) {
-//         console.log(error)
-//       }
-//     },
-//   },
-// })
+        const newFilm = await axios.post(`https://ghiblipediaapi.onrender.com/api/movies/omdb/${filmTitle}`)
+        this.films = newFilm.data
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+})
 </script>
 <template>
   <div>watchlist</div>
